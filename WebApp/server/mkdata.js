73ee945ef75b904f12b8343157a3bc806a758ae3
wbolen <47831381+wbolen@users.noreply.github.com>
2021-01-28 11:43:45 -0500

    Create mkdata.js

---
+++ WebApp/server/mkdata.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Mask = require('./lib/models/mask');
+
+const Bw = new Mask({
+  section:'Bw',
+  name: "Black and white",
+  price: "10",
+  size: "Adult",
+  description: "black and white mask"
+});
+
+const Wh = new Mask({
+  section:'Wh',
+  name: "White",
+  price: "10",
+  size: "Adult",
+  description: "white mask"
+});
+const Gr = new Mask({
+  section:'Gr',
+  name: "Gray",
+  price: "10",
+  size: "Kids",
+  description: "Gray mask"
+});
+const Rd = new Mask({
+  section:'Rd',
+  name: "Red",
+  price: "10",
+  size: "Adult",
+  description: "Red mask"
+});
+
+mongoose.connect('mongodb://localhost:27017/ProjectWebApp', {useNewUrlParser: true, useUnifiedTopology: true});
+
+Bw.save().catch(err => console.error(err)).then(() => { console.log('saved bw') });
+Wh.save().catch(err => console.error(err)).then(() => { console.log('saved wh') });
+Gr.save().catch(err => console.error(err)).then(() => { console.log('saved gr') });
+Rd.save().catch(err => console.error(err)).then(() => { console.log('saved rd') });
