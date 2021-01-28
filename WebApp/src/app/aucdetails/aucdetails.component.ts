17e6e35b1b1d0ef84e3d45e06becd715a3878347
wbolen <47831381+wbolen@users.noreply.github.com>
2021-01-28 11:43:24 -0500

    Create aucdetails.component.ts

---
+++ WebApp/src/app/aucdetails/aucdetails.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MaskDataService} from '../mask-data.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-aucdetails',
+  templateUrl: './aucdetails.component.html',
+  styleUrls: ['./aucdetails.component.css']
+})
+export class AucdetailsComponent implements OnInit {
+
+  maskNames: Observable<string[]>;
+
+  constructor(
+    private maskDataService: MaskDataService
+  ) { }
+
+  ngOnInit(): void {
+    this.maskNames = this.maskDataService.getMaskName();
+  }
+
+}
