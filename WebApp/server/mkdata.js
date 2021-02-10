8c4b3edc17988e8354cd20a65afff908f5ab578f
wbolen <47831381+wbolen@users.noreply.github.com>
2021-02-09 22:24:42 -0500

    Update mkdata.js

--- WebApp/server/mkdata.js
+++ WebApp/server/mkdata.js
@@ -3,53 +3,73 @@ const Mask = require('./lib/models/mask');
 
 const M10 = new Mask({
   id: '10',
-  name: 'Mask10',
-  price : '$19.99'
+  name: 'Funky Feathers',
+  price : '$19.99',
+  description:"Feathery Look on the Outside / Royal Blue Inside",
+  fit: "Adult"
 });
 const M9 = new Mask({
   id: '9',
-  name: 'Mask9',
-  price : '$19.99'
+  name: 'Black Stripes',
+  price : '$19.99',
+  description:"Black and White Stripped Outside / White Inside",
+  fit: "Adult"
 });
 const M8 = new Mask({
   id: '8',
-  name: 'Mask8',
-  price : '$19.99'
+  name: 'Anchors',
+  price : '$19.99',
+  description:"Yellow and White Anchor Outside / Red Inside",
+  fit: "Adult"
 });
 const M7 = new Mask({
   id: '7',
-  name: 'Mask7',
-  price : '$19.99'
+  name: 'Cars',
+  price : '$19.99',
+  description:"Cars Themed Outside / Cars Themed Inside",
+  fit: "Kids"
 });
 const M6 = new Mask({
   id: '6',
-  name: 'Mask6',
-  price : '$19.99'
+  name: 'Red Plaid',
+  price : '$19.99',
+  description:"Red Plaid Outside / White Inside",
+  fit: "Adult"
 });
 const M5 = new Mask({
   id: '5',
-  name: 'Mask5',
-  price : '$19.99'
+  name: 'Lightning McQueen',
+  price : '$19.99',
+  description:"Lightning McQueen Outside / Green Plaid Inside",
+  fit: "Kids"
 });
 const M4 = new Mask({
   id: '4',
-  name: 'Mask4',
-  price : '$19.99'
+  name: 'Diamonds',
+  price : '$19.99',
+  description:"Green and Blue Diamond Outside / Purple Inside",
+  fit: "Adult"
 });
 const M3 = new Mask({
   id: '3',
-  name: 'Mask3',
-  price : '$19.99'
+  name: 'RB Plaid',
+  price : '$19.99',
+  description:"Red and Black Plaid Outside / White Inside",
+  fit: "Adult"
 });
 const M2 = new Mask({
   id: '2',
-  name: 'Mask2',
-  price : '$19.99'
+  name: 'Buffalo Plaid',
+  price : '$19.99',
+  description:"Buffalo Plaid outside / White Inside",
+  fit: "Adult"
 });
 const M1 = new Mask({
   id: '1',
-  name: 'Mask1',
-  price : '$19.99'
+  name: 'Flowers',
+  price : '$19.99',
+  description:"Black and White Flower Outside / Cactus Inside",
+  fit: "Adult"
 });
 
 mongoose.connect('mongodb://localhost:27017/ProjectWebApp', {useNewUrlParser: true, useUnifiedTopology: true});
