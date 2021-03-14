5c6daa6d78282b0e5217ab8c3a2376ccc2fe2ee8
Ben Szabo <bcszabo@iu.edu>
2021-03-14 17:24:45 -0400

    test
    test

--- mangars-masks-frontEnd/src/app/models/ITransactionItem.ts
+++
@@ -1,8 +0,0 @@
-import {IUnitAmount} from "./IUnitAmount";
-
-export class ITransactionItem {
-  public name: string;
-  // tslint:disable-next-line:variable-name
-  public unit_amount: IUnitAmount;
-  public quantity: string;
-}
