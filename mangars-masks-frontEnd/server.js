814bc03f3e707165f1ee74a136d83439f0dee5c7
kmangar <kham.mangar@outlook.com>
2021-03-14 14:49:48 -0400

    server changes

--- mangars-masks-frontEnd/server.js
+++ mangars-masks-frontEnd/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + 'src/app'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(dirname + '/dist'));
+
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/index.html'));});
-app.listen(process.env.PORT || 8080)
+
+  res.sendFile(path.join(dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
