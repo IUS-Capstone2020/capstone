1cde6be7f85c3d5b4fd20d89b52855ae32246ae8
kmangar <kham.mangar@outlook.com>
2021-03-14 14:28:20 -0400

    server changes

--- mangars-masks-frontEnd/server.js
+++ mangars-masks-frontEnd/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-simple-shopping-cart'));
+app.use(express.static(__dirname + 'src/app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/angular-simple-shopping-cart/index.html'));});
+    '/dist/index.html'));});
 app.listen(process.env.PORT || 8080)
