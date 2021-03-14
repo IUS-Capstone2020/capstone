57bea0554385bb19b3c001a41a7ffd86bd3783eb
Ben Szabo <bcszabo@iu.edu>
2021-03-14 17:01:46 -0400

    additions for deployment
    additions for deployment

--- mangars-masks-api/src/main/java/com/appApi/controller/MaskController.java
+++ mangars-masks-api/src/main/java/com/appApi/controller/MaskController.java
@@ -11,7 +11,7 @@ import java.util.List;
 /*
 @CrossOrigin(origins = {"http://localhost:4200"})
 */
-@CrossOrigin(origins = {"https://sleepy-sands-09473.herokuapp.com"})
+@CrossOrigin(origins = {"https://iuscapstoneproject2020.herokuapp.com/"})
 @RequestMapping("api/v1/")
 public class MaskController {
 	
